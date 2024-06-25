ff8a15d687e1992107a493dea332031b587ad524
vashtileonie <vashti.delapaz@gmail.com>
2024-06-25 22:56:29 +0800

    implemented inheritance and polymorphism

---
+++ mini-project-2/src/main/java/org/stratpoint/project2/vbauson/Book.java
@@ -0,0 +1,39 @@
+package org.stratpoint.project2.vbauson;
+
+class Book extends Category {
+    private String title, author;
+    private int isbn;
+
+    public Book(String title, String author, int isbn,  String type) {
+        super(type);
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    //getter and setters
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
