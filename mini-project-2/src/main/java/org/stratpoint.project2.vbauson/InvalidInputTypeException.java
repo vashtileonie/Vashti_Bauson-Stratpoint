5d0325666304f95cc78d056e54bd4a67c50839c9
vashtileonie <vashti.delapaz@gmail.com>
2024-06-26 16:56:19 +0800

    fixing structure

--- mini-project-2/src/main/java/org/stratpoint.project2.vbauson/InvalidInputTypeException.java
+++
@@ -1,7 +0,0 @@
-package org.stratpoint.project2.vbauson;
-
-public class InvalidInputTypeException extends Exception {
-    public InvalidInputTypeException(String message) {
-        super(message);
-    }
-}
--- mini-project-2/src/main/java/org/stratpoint.project2.vbauson/LibraryInterface.java
+++
@@ -1,7 +0,0 @@
-package org.stratpoint.project2.vbauson;
-
-public interface LibraryInterface {
-    void addBook(Book book);
-    void deleteBook(int isbn);
-    void searchBook(String title);
-}
