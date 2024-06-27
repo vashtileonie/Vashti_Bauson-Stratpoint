65f2f7e3391a2cecaab564ca47bca8efca2b28a4
vashtileonie <vashti.delapaz@gmail.com>
2024-06-27 09:24:27 +0800

    added more comments

--- mini-project-2/src/test/java/com/vashtibauson/library/service/Impl/DigitalLibraryServiceImplTest.java
+++ mini-project-2/src/test/java/com/vashtibauson/library/service/Impl/DigitalLibraryServiceImplTest.java
@@ -8,35 +8,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DigitalLibraryServiceImplTest {
 
+    // Instance of the class being tested
     private DigitalLibraryServiceImpl digitalLibraryService;
 
+    // Setup method to initialize objects before each test
     @BeforeEach
     void setUp() {
         digitalLibraryService = new DigitalLibraryServiceImpl();
-        System.out.println("TEST FOR: E-BOOK");
     }
 
+    // Test method for adding an eBook
     @Test
     void addEBook() {
+        // Create a new EBook object
         EBook eBook = new EBook("E-Book", "E-Book1", "George Orwell", 789012, 5.5, "PDF");
+
+        // Add the eBook to the digital library
         digitalLibraryService.addEBook(eBook);
+
+        // Verify that the eBook list size is now 1
         assertEquals(1, digitalLibraryService.eBookList.size());
+
+        // Verify that the title of the first eBook in the list is "E-Book1"
         assertEquals("E-Book1", digitalLibraryService.eBookList.get(0).getTitle());
     }
 
+    // Test method for deleting an eBook
     @Test
     void deleteEBook() {
+        // Create a new EBook object
         EBook eBook = new EBook("E-Book", "Harry Potter: Goblet of Fire", "JK Rowling", 456789, 8, "TXT File");
+
+        // Add the eBook to the digital library
         digitalLibraryService.addEBook(eBook);
+
+        // Delete the eBook with the specified ISBN
         digitalLibraryService.deleteEBook(456789);
+
+        // Verify that the eBook list size is now 0
         assertEquals(0, digitalLibraryService.eBookList.size());
     }
 
+    // Test method for searching an eBook by title
     @Test
     void searchEBook() {
+        // Create a new EBook object
         EBook eBook = new EBook("E-Book", "E-Book2", "Juan Dela Cruz", 345786, 5.5, "PDF");
+
+        // Add the eBook to the digital library
         digitalLibraryService.addEBook(eBook);
+
+        // Search for the eBook with the specified title
         digitalLibraryService.searchEBook("E-Book2");
+
+        // Verify that the title of the first eBook in the list is "E-Book2"
         assertEquals("E-Book2", digitalLibraryService.eBookList.get(0).getTitle());
     }
 }
--- mini-project-2/src/test/java/com/vashtibauson/library/service/Impl/LibraryServiceImplTest.java
+++ mini-project-2/src/test/java/com/vashtibauson/library/service/Impl/LibraryServiceImplTest.java
@@ -7,40 +7,61 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryServiceImplTest {
+
+    // Instance of the class being tested
     private LibraryServiceImpl libraryService;
 
-
+    // Setup method to initialize objects before each test
     @BeforeEach
     public void setUp() {
         libraryService = new LibraryServiceImpl();
-
     }
 
+    // Test method for adding a book
     @Test
     void addBook() {
+        // Create a new Book object
         Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, "Book");
 
+        // Add the book to the library
         libraryService.addBook(book);
+
+        // Verify that the book list size is now 1
         assertEquals(1, libraryService.bookList.size());
+
+        // Verify that the title of the first book in the list is "The Great Gatsby"
         assertEquals("The Great Gatsby", libraryService.bookList.get(0).getTitle());
     }
 
-
+    // Test method for deleting a book
     @Test
     void deleteBook() {
+        // Create a new Book object
         Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, "Book");
 
+        // Add the book to the library
         libraryService.addBook(book);
 
+        // Delete the book with the specified ISBN
         libraryService.deleteBook(123456);
+
+        // Verify that the book list size is now 0
         assertEquals(0, libraryService.bookList.size());
     }
 
+    // Test method for searching a book by title
     @Test
     void searchBook() {
+        // Create a new Book object
         Book book = new Book("Harry Potter: Goblet of Fire", "JK Rowling", 456789, "Book");
+
+        // Add the book to the library
         libraryService.addBook(book);
+
+        // Search for the book with the specified title keyword
         libraryService.searchBook("Harry Potter");
-            assertEquals("Harry Potter: Goblet of Fire", libraryService.bookList.get(0).getTitle());
+
+        // Verify that the title of the first book in the list contains "Harry Potter"
+        assertEquals("Harry Potter: Goblet of Fire", libraryService.bookList.get(0).getTitle());
     }
 }
