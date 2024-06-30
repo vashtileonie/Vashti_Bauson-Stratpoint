c068d792ef1c32b6548d33d9f5166853d2de1f0a
vashtileonie <vashti.delapaz@gmail.com>
2024-06-30 23:53:23 +0800

    added lambda expression and modified documentation

--- mini-project-3/src/main/java/com/vashtibauson/cart/service/impl/CartImpl.java
+++ mini-project-3/src/main/java/com/vashtibauson/cart/service/impl/CartImpl.java
@@ -1,6 +1,5 @@
 package com.vashtibauson.cart.service.impl;
 
-import com.vashtibauson.cart.Main;
 import com.vashtibauson.cart.model.Product;
 import com.vashtibauson.cart.service.Cart;
 
@@ -38,7 +37,7 @@ public class CartImpl implements Cart {
         // Print confirmation message to console
         System.out.println("Added " + product.getName() + " to the cart.");
         // Log the addition of an item
-       logger.info("Item Added");
+        logger.info("Item Added");
     }
 
     /**
@@ -46,43 +45,34 @@ public class CartImpl implements Cart {
      */
     public void viewCart() {
         if (items.isEmpty()) {
-            // Print message if cart is empty
             System.out.println("The cart is empty.");
-            // Log that the cart is empty
             logger.info("Cart is empty.");
             return;
         }
-        // Print heading for cart items
+
         System.out.println("Cart items:");
-        // Format numbers to show currency properly
         NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US);
         formatter.setMinimumFractionDigits(2);
         formatter.setMaximumFractionDigits(2);
-        // Iterate through items and print each with formatted price
-        for (Product item : items) {
-            System.out.printf("- %s: PHP%s%n", item.getName(), formatter.format(item.getPrice()));
-            // Log that the cart is displayed
-            //logger.info("Cart is displayed.");
-        }
+
+        // Iterate through items using lambda expression to print each item with formatted price
+        items.forEach(item -> System.out.printf("- %s: PHP%s%n", item.getName(), formatter.format(item.getPrice())));
     }
 
     /**
      * Calculates and prints the total price of all items in the cart.
      */
     public void calculateTotal() {
-        double total = 0;
-        // Calculate total price by summing up each item's price
-        for (Product item : items) {
-            total += item.getPrice();
-        }
-        // Format total price for proper currency display
+        // Use stream and lambda expression to calculate total price
+        double total = items.stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+
         NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US);
         formatter.setMinimumFractionDigits(2);
         formatter.setMaximumFractionDigits(2);
-        // Print total price with currency symbol
+
         System.out.println("Total price: PHP" + formatter.format(total));
-        // Log that total is calculated
-       // logger.info("Total is calculated.");
     }
 
     /**
