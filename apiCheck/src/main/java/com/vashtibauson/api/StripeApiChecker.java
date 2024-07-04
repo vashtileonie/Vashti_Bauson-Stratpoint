a77a1f3505746fea6e8e8b1ca04c579ab560a5e7
vashtileonie <vashti.delapaz@gmail.com>
2024-07-04 16:31:23 +0800

    fixed dependency

--- apiCheck/src/main/java/com/vashtibauson/api/StripeApiChecker.java
+++ apiCheck/src/main/java/com/vashtibauson/api/StripeApiChecker.java
@@ -1,12 +1,12 @@
 package com.vashtibauson.api;
+
 import com.stripe.Stripe;
 import com.stripe.model.Balance;
 
 public class StripeApiChecker {
     public static void main(String[] args) {
         // Set your Stripe API key
-        String apiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"; // Replace with your actual Stripe Secret key
-
+        String apiKey = "sk_test_51PYhYURoq8wyEEyAN6FXGGPXkeXfDwl0akW8QpCCHANnYQKiwUAZAemn0vcrmzvU7493L6bFaSPVniHzULzwM2Ov00SIOnBc3X";
         try {
             // Initialize the Stripe API with your key
             Stripe.apiKey = apiKey;
